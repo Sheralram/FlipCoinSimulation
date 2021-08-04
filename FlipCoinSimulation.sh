b300856120c00d502be47dbe29cdcdc40b1856fd
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-05 00:04:49 +0530

    Flipping a Coin till one reaches to 21

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Simulation Program"
@@ -16,11 +15,29 @@ do
 	then
 		echo "HEADS"
 		Hcount=$(($Hcount + 1))
-	else
+
+             if [ $Hcount == 21 ]
+		then
+			break
+		fi
+       else
 		echo "TAILS"
 		Tcount=$(($Tcount + 1))
+                 if [ $Tcount == 21 ]
+		then
+			break
+		fi
 	fi
 done
 
 echo "Head = $Hcount"
 echo "Tail = $Tcount"
+ 
+if [ $Hcount -gt $Tcount ]
+then 
+   Head=$(( ($Hcount)-($Tcount) ))
+echo "Head Win by $Head times"
+else
+Tail=$(( ($Tcount)-($Hcount) ))
+echo "Tail Win by $Tail times"
+fi
